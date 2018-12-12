f9ebb283dc2373e77fe852af30625277cca6bea2
Ruby Chaube <ruby.19april@gmail.com>
2018-12-12 16:32:09 +0530

    .
    .

--- TRAINING/SPRING-WS/Spring110_MVCBasics/src/com/jp/hr/controllers/HomePageController.java
+++ TRAINING/SPRING-WS/Spring110_MVCBasics/src/com/jp/hr/controllers/HomePageController.java
@@ -1,15 +1,23 @@
 package com.jp.hr.controllers;
 
 import java.util.ArrayList;
+import java.util.Set;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
-
 import com.jp.hr.entities.Employee;
 import com.jp.hr.exceptions.HrException;
 import com.jp.hr.services.ServiceEmployee;
@@ -31,6 +39,18 @@ public class HomePageController {
 	@Autowired
 	@Qualifier("service") // This is will do autowiring for serviceLayer
 	private ServiceEmployee empService;
+	
+	@Autowired
+	private Validator validator;
+	
+	
+	@InitBinder
+	private void validatorBinder() {
+		// This is Java Service Request JSR 303 validator
+		ValidatorFactory validatorFactory = Validation.buildDefaultValidatorFactory();
+        validator = validatorFactory.getValidator();      
+        System.out.println("Validator is set."+validator.hashCode());
+	}
 	
 	@RequestMapping("homePage.hr")  // This is the url(command)  for getHomePage method 
 	public String getHomePage() {
@@ -86,15 +106,33 @@ public class HomePageController {
 	}
 	
 	@RequestMapping("submitRegistrationData.hr") // This is the jsp name mentioned as a link name in emplist.jsp
-	public String submitRegistrationData(@ModelAttribute("command") Employee emp, Model model) {
+	public String submitRegistrationData(@ModelAttribute("command") Employee emp,  BindingResult result, Model model) {
 		System.out.println(emp);		
-		try {
-			empService.addNewEmployee(emp);
-			return "SaveSuccess";
-		} catch (HrException e) {
-			model.addAttribute("msg", "Insert failed." + e.getMessage());
-			return "EntryPage";
-		}
-				
+		// We have apply Validation here		
+		Set<ConstraintViolation<Employee>> violations = validator.validate(emp);
+		
+		for (ConstraintViolation<Employee> violation : violations)
+        {
+            String propertyPath = violation.getPropertyPath().toString();
+            String message = violation.getMessage();
+            // Add JSR-303 errors to BindingResult. This allows Spring to display them in view via a FieldError
+            FieldError error = new FieldError("command",propertyPath,
+                    "Invalid "+ propertyPath + "(" + message + ")");
+            result.addError(error);
+        }		
+		
+    	if (result.hasErrors()) {
+    		model.addAttribute("msg", "Error in entry.");    		
+            return "EntryPage";
+        } else {
+        	try {
+    			empService.addNewEmployee(emp);
+    			return "SaveSuccess";
+    		} catch (HrException e) {
+    			model.addAttribute("msg", "Insert failed." + e.getMessage());
+    			return "EntryPage";
+    		}
+        }
+		
 	}
 }
