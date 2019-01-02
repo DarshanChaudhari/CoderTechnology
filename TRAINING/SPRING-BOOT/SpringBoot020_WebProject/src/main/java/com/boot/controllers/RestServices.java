029c07360eba1c24533f9bf5f80233a79a8c1894
Darshan Chaudahri <darshan.chaudhari10@gmail.com>
2019-01-02 20:51:06 +0530

    .
    .

---
+++ TRAINING/SPRING-BOOT/SpringBoot020_WebProject/src/main/java/com/boot/controllers/RestServices.java
@@ -0,0 +1,18 @@
+package com.boot.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController; 
+
+@RestController 
+//@EnableAutoConfiguration
+public class RestServices {
+
+	public RestServices(){
+		System.out.println("Bean created.");
+	}
+	@RequestMapping("/hello")  
+    public String hello(){  
+        return"Hello!";  
+    } 
+	
+}
